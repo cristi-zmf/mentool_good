07ae504e4315980d49f4c6cb7134512a504aade8
Cristi <cristi_zmf@yahoo.com>
2019-01-27 22:47:03 +0200

    added eureka

--- src/main/java/com/cristi/mentool/mentool/MentoolApplication.java
+++ src/main/java/com/cristi/mentool/mentool/MentoolApplication.java
@@ -3,9 +3,11 @@ package com.cristi.mentool.mentool;
 import com.cristi.mentool.mentool.infra.configurations.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableEurekaClient
 @Import(AppConfiguration.class)
 public class MentoolApplication {
 	public static void main(String[] args) {
