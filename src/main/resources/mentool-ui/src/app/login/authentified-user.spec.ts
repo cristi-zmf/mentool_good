82fc04cd7451b5543cde81e39d6b0fc17bbfc56b
cristi-zmf <cristi_zmf@yahoo.com>
2019-01-31 21:23:39 +0200

    removed ui as it was moved to other project

--- src/main/resources/mentool-ui/src/app/login/authentified-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthentifiedUser } from './authentified-user';
-
-describe('AuthentifiedUser', () => {
-  it('should create an instance', () => {
-    expect(new AuthentifiedUser()).toBeTruthy();
-  });
-});
